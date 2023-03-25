b60d9417f772968281a6b6bc285b1a9ed38972f4
xMaxrayx <101531362+xmaxrayx@users.noreply.github.com>
2023-03-25 12:53:24 +0100

    .

---
+++ Console/Hello_world/Hello_world_NT/Hello_world_NT/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hello_world_NT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, fellowing weabbo-chan");
+            Console.ReadLine();
+        }
+    }
+}
+
+//gg wp 
