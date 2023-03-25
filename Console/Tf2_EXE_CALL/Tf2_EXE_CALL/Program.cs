18293e83695396359613839bc47a8d3b20cc307c
xMaxrayx <101531362+xmaxrayx@users.noreply.github.com>
2023-03-25 11:48:04 +0100

    .

---
+++ Console/Tf2_EXE_CALL/Tf2_EXE_CALL/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using System.Diagnostics;
+Process.Start("F:\\SteamLibrary\\steamapps\\common\\Titanfall2\\Titanfall2Launcher.bat");
+Console.WriteLine("start");
+Console.WriteLine("Hello, World!");
+Thread.Sleep(5000); //will sleep for 5 sec
+
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
+
+/*
+
+
+class Program
+{
+    static void Main()
+    {
+        Process.Start(@"F:\SteamLibrary\steamapps\common\Titanfall2\Titanfall2Launcher.bat");
+    }
+}
+
+*/
+
+
+/*
+{
+    var key = Console.ReadKey();
+    if (key.Key == ConsoleKey.Escape)
+    {
+        Environment.Exit(0);
+    }
+}
+*/
