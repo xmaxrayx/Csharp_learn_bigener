3ebb707b9981b062748210a91eeae5ad217003a3
xMaxrayx <101531362+xmaxrayx@users.noreply.github.com>
2023-03-26 10:04:21 +0200

    .

---
+++ Console/Hello_world/Hello_world_2/Hello_world_2/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace Hello_world_2 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //varibale
+            String Greating = "Hi ,How are you? ^^";
+            String Name = ("Sir");
+
+            Console.WriteLine("");
+            Console.WriteLine(Greating);
+            Console.WriteLine(Name + ", Whold you like drink a cub of japaneses tea from my Miku?");
+            
+            //varaibale
+            int Cup_Count = (2);
+            float Cup_Price = (4.54f);
+
+            //
+            Console.WriteLine();
+            Console.WriteLine("Totel Price" + " $");
+            Console.WriteLine(Cup_Price * Cup_Count);
+
+            //var
+            int a = 1;
+            int b = 4;
+            
+            int c = a + b;
+            Console.WriteLine("Old Result was :" + c);
+
+            a = 5;
+            c = a + b;
+            Console.WriteLine("New Result is :" + c);
+
+
+
+        }   
+
+    }
+}
--- Console/Hello_world/Hello_world_NT/Hello_world_NT/Program.cs
+++ Console/Hello_world/Hello_world_NT/Hello_world_NT/Program.cs
@@ -6,7 +6,7 @@ namespace Hello_world_NT
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi, fellowing weabbo-chan");
+            Console.WriteLine("Hi, fellowing weeebbo");
             Console.ReadLine();
         }
     }
